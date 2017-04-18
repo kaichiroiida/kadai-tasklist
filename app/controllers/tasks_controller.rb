31d2852a6e6db5f371d238e59804fd798012269d
kaichiroiida <s15mm302@gmail.com>
2017-04-18 04:09:02 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,8 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  
   def index
     @tasks = Task.order(created_at: :desc).page(params[:page]).per(12)
   end
@@ -43,7 +46,7 @@ class TasksController < ApplicationController
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
-end
+
 
   private
 
@@ -54,3 +57,14 @@ end
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  def task_params
+    params.require(:task).permit(:content)
+  end
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_path
+    end
+  end
+end
---
+++ db/migrate/20170418022439_droptask.rb
@@ -0,0 +1,5 @@
+class Droptask < ActiveRecord::Migration[5.0]
+  def change
+    drop_table :tasks
+  end
+end
