6a3847fdca0ff39b2da257e4159606c3e79d014a
kaichiroiida <s15mm302@gmail.com>
2017-04-18 01:55:34 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   get 'users/index'
 
   get 'users/show'
@@ -8,6 +14,11 @@ Rails.application.routes.draw do
   get 'users/create'
 
     root to: 'toppages#index'
+    
+    
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
     
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
